e5994899838d456a03a5eec9e1cdd643d7af9e90
RileyManda <rileymanda69@gmail.com>
2022-01-14 14:31:59 +0200

    create bookmark input fields|code cleanup

--- src/components/BookMarkHeader/BookMarkHeader.js
+++ src/components/BookMarkHeader/BookMarkHeader.js
@@ -1,24 +1,15 @@
 import { useRef, useState, useCallback } from 'react';
 // Material
-import { styled, alpha } from '@mui/material/styles';
 import Box from '@mui/material/Box';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import InputBase from '@mui/material/InputBase';
-import MenuIcon from '@mui/icons-material/Menu';
-import SearchIcon from '@mui/icons-material/Search';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import PopupState, { bindTrigger, bindPopover } from 'material-ui-popup-state';
 import Popover from '@mui/material/Popover';
+import TextField from '@mui/material/TextField';
 
-
-import addBookMark from '@iconify/icons-eva/file-add-fill';
 import editBookMark from '@iconify/icons-eva/edit-2-fill';
-import roundClearAll from '@iconify/icons-ic/round-clear-all';
-import roundFilterList from '@iconify/icons-ic/round-filter-list';
 import { Icon } from '@iconify/react';
-import CreateBookMark from '../CreateBookMark';
+
 
 
 export default function BookMarkHeader() {
@@ -45,7 +36,23 @@ export default function BookMarkHeader() {
               transformOrigin={{
               vertical: 'top',
               horizontal: 'center'}}>
-              <Typography sx={{ p: 2 }}>Create BookMark Form</Typography>
+                <Box
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+        <Stack direction="row" spacing={2}>
+
+        <TextField id="outlined-basic" label="Title" variant="outlined" />
+  <TextField id="outlined-basic" label="Description" variant="outlined" />
+        </Stack>
+
+
+    </Box>
+              
             </Popover>
           </div>
         )}
